6e1604f0e476973a29119887eac7f97e4073315c
Galhoom <102315329+Galhoom22@users.noreply.github.com>
2025-10-13 07:17:31 +0300

    feat(filter): add event type filtering via command-line argument

--- App.php
+++ App.php
@@ -10,11 +10,21 @@ if (!isset($argv[1])) {
     $formatter = new ActivityFormatter();
 
     $username = $argv[1];
+    if(isset($argv[2])){
+        $filter = explode('=', $argv[2]);
+        $filterType = $filter[1];
+    }
     $response = $api->getUserActivity($username);
 
     echo "Fetching for user activity: " . $username . "\n";
     $events = json_decode($response, true);
     foreach($events as $event){
-        echo $formatter->formatEvent($event) . "\n";
+        if(!isset($filterType)){
+            echo $formatter->formatEvent($event) . "\n";
+        }else{
+            if($event['type'] === $filterType){
+                echo $formatter->formatEvent($event) . "\n";
+            }
+        }
     }
 }
